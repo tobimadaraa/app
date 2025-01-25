77993c77ccea63ef5395fb5402d581cbc59f4ac6
Eung <madara540@gmail.com>
2025-01-25 17:09:18 +0200

    search errors fixed

--- lib/components/report_button.dart
+++ lib/components/report_button.dart
@@ -3,54 +3,60 @@ import 'package:flutter_application_2/models/user_model.dart';
 import 'package:flutter_application_2/repository/user_repository.dart';
 import 'package:get/get.dart';
 
-class ReportButton extends StatelessWidget {
+class ReportButton extends StatefulWidget {
   final String newUserId;
   final String newTagLine;
   final Future<void> Function() onSuccess;
 
   const ReportButton({
-    Key? key,
+    super.key,
     required this.newUserId,
     required this.newTagLine,
     required this.onSuccess,
-  }) : super(key: key);
+  });
 
-  void _handleReport(BuildContext context) {
-    if (newUserId.isNotEmpty && newTagLine.isNotEmpty) {
+  @override
+  ReportButtonState createState() => ReportButtonState();
+}
+
+class ReportButtonState extends State<ReportButton> {
+  Future<void> _handleReport() async {
+    if (widget.newUserId.isNotEmpty && widget.newTagLine.isNotEmpty) {
       final user = UserModel(
-        userId: newUserId,
-        tagLine: newTagLine,
+        userId: widget.newUserId,
+        tagLine: widget.newTagLine,
         timesReported: 0,
         lastReported: DateTime.now(),
       );
 
-      Get.find<UserRepository>()
-          .createUser(user)
-          .then((_) async {
-            await onSuccess(); // Trigger the onSuccess callback
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('User data submitted successfully!'),
-              ),
-            );
-          })
-          .catchError((error) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Failed to submit user data!')),
-            );
-          });
+      try {
+        await Get.find<UserRepository>().createUser(user);
+        if (mounted) {
+          await widget.onSuccess(); // Trigger the success callback
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('User data submitted successfully!')),
+          );
+        }
+      } catch (error) {
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Failed to submit user data!')),
+          );
+        }
+      }
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Please enter both Riot ID and Tagline')),
-      );
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Please enter both Riot ID and Tagline'),
+          ),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => _handleReport(context),
-      child: const Text("Report"),
-    );
+    return TextButton(onPressed: _handleReport, child: const Text("Report"));
   }
 }
