c3b84bff11143d0d7fb3270700357b32b830ad2b
Eung <madara540@gmail.com>
2025-01-26 01:48:04 +0200

    big money

--- lib/pages/buttons/activity_button.dart
+++ lib/pages/buttons/activity_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/classes/classm.dart';
+import 'package:flutter_application_2/classes/colour_classes.dart';
 
 class Activitybutton extends StatelessWidget {
   final String text;
--- lib/pages/buttons/logout_button.dart
+++ lib/pages/buttons/logout_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/classes/classm.dart';
+import 'package:flutter_application_2/classes/colour_classes.dart';
 
 class Logoutbutton extends StatelessWidget {
   final String text;
