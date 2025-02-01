17a2ce359135ee561593a2530b654a143a1f0a62
Eung <madara540@gmail.com>
2025-02-02 01:40:19 +0200

    \

--- lib/Screens/dodge_list_screen.dart
+++ lib/Screens/dodge_list_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/components/dodge_list_controller.dart';
 import 'package:flutter_application_2/models/leaderboard_model.dart';
