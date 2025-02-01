09baa63d0a2579c8702697160f53f8afc3a5a02e
Eung <madara540@gmail.com>
2025-02-01 19:07:15 +0200

    add different languages

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -1,21 +1,21 @@
 class Validator {
-  static const _taglineRegex = r'^[a-zA-Z0-9]{1,6}$';
-  static const _usernameRegex = r'^[a-zA-Z0-9]{1,16}$';
-  // static const _pureNumbersRegex = r'^[0-9]+$';
+  // This pattern allows Unicode characters in the range U+00C0 to U+D7FF
+  // as well as ASCII letters and digits.
+  static const validCharPattern = r'^[\u00C0-\uD7FF0-9A-Za-z]+$';
 
   static String? validateTagline(String value) {
     if (value.isEmpty) return 'Tagline is required';
-    if (value.length < 3 || value.length > 5) return 'Enter 3-5 Characters';
-    //  if (RegExp(_pureNumbersRegex).hasMatch(value)) return 'No pure numbers';
-    if (!RegExp(_taglineRegex).hasMatch(value)) return 'Invalid characters';
+    // Enforce a length of 3-5 characters
+    if (value.length < 3 || value.length > 5) return 'Enter 3-5 characters';
+    if (!RegExp(validCharPattern).hasMatch(value)) return 'Invalid characters';
     return null;
   }
 
   static String? validateUsername(String value) {
     if (value.isEmpty) return 'Riot ID is required';
-    if (value.length < 3 || value.length > 16) return 'Enter 3-16 Characters';
-    //   if (RegExp(_pureNumbersRegex).hasMatch(value)) return 'No pure numbers';
-    if (!RegExp(_usernameRegex).hasMatch(value)) return 'Invalid characters';
+    // Enforce a length of 3-16 characters
+    if (value.length < 3 || value.length > 16) return 'Enter 3-16 characters';
+    if (!RegExp(validCharPattern).hasMatch(value)) return 'Invalid characters';
     return null;
   }
 }
