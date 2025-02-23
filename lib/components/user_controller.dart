bd5c9940ef8b49235f1b9c52afb9baff6add4241
Eung <madara540@gmail.com>
2025-02-23 20:10:35 +0200

    sss

--- lib/components/user_controller.dart
+++ lib/components/user_controller.dart
@@ -1,9 +1,8 @@
 import 'package:get/get.dart';
 
 class UserController extends GetxController {
-  // Create an observable boolean for premium status.
-  // Initially false; update this value when the user becomes premium.
-  var isPremium = false.obs;
+  // Create an observable boolean
+  var isPremium = true.obs;
 
   // You can add additional user properties here.
 }
