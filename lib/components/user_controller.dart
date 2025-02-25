82b77c1409f03f9056c275a46235d869f23e1b0b
Eung <madara540@gmail.com>
2025-02-25 03:30:52 +0200

    f9ixed pw

--- lib/components/user_controller.dart
+++ lib/components/user_controller.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 class UserController extends GetxController {
   // Create an observable boolean
-  var isPremium = false.obs;
+  var isPremium = true.obs;
 // check for ads watched
   // You can add additional user properties here.
 }
